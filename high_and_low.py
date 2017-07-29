f16ddbde76bc827d46b42eb7827d6bca7349757a
ColmeSH <mauriziocontatto@gmail.com>
2017-07-29 18:51:29 +0200

    init logic exercises in python repository

---
+++ high_and_low.py
@@ -0,0 +1,10 @@
+def high_and_low(numbers):
+  if not numbers:
+    return 'list empty'
+  else:
+    l = numbers.split(' ')
+    result = [ int(number) for number in l ]
+    return (max(result),min(result))
+
+print high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6")
+print high_and_low([])
