6c89fb45ea4366552443329b3a1109257e674e75
ColmeSH <mauriziocontatto@gmail.com>
2017-08-05 00:11:53 +0200

    update find missing letter method

--- find_missing_letter.py
+++ find_missing_letter.py
@@ -1,12 +1,30 @@
 def find_missing_letter(chars):
+  chars.sort()
   alphabet = 'abcdefghijklmnopqrstuvwxyz'
   if type(chars) != list:     
     print 'ERR: not list passed'
-  else:
+  elif len(chars) >= 2:
+    if alphabet == ''.join(chars): 
+      return 'is the same'
+    start_point = alphabet.index(chars[0].lower())
+    if start_point > 1:
+      alphabet = alphabet[start_point::]
     for i, elem in enumerate(alphabet):
-      if elem.lower() != chars[i]:
+      if start_point > 1:
+        elem = elem.upper()
+      if elem != chars[i]:
         return elem
+  else:
+    return 'need a list of at least 2 elements'
       
         
-
-print find_missing_letter(['a','b','c','d','f'])
+# first case
+print 'CASE 1:', find_missing_letter(['a','b','c','d','f'])
+# second case
+print 'CASE 2:', find_missing_letter(['O','Q','R','S','T'])
+# third case
+print 'CASE 3:', find_missing_letter(['b','c','d','e','f'])
+# fourth case
+print 'CASE 4: ',find_missing_letter(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
+# fifth case
+print 'CASE 5: ',find_missing_letter(['a'])
